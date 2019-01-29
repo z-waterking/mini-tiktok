422164abc5729b36439f8cb03522114ea8abd912
tiyarest <2052476174@qq.com>
2019-01-29 11:09:00 +0800

    aboutme

--- zhuzai/app/src/main/java/com/zhuzai/homework/zhuzai/mePage/MePage.java
+++ zhuzai/app/src/main/java/com/zhuzai/homework/zhuzai/mePage/MePage.java
@@ -1,7 +1,9 @@
 package com.zhuzai.homework.zhuzai.mePage;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.media.Image;
 import android.os.Bundle;
@@ -10,7 +12,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
@@ -36,6 +41,15 @@ public class MePage extends BaseFragment {
     private ImageView line_view;
     private String my_user_id;
 
+    private Button edit_btn;
+    private SharedPreferences mPreferences;
+    private SharedPreferences.Editor mEditor;
+    private TextView mypage_name;
+    private Button commit;
+    private EditText name;
+    private EditText fale;
+    private EditText say;
+
     //    public static MainActivity mainactivity;
     private View view;
     @Override
@@ -51,6 +65,50 @@ public class MePage extends BaseFragment {
         my_user_id = "865873038279224";
         initView();
         getUserDraw();
+
+        edit_btn = view.findViewById(R.id.mypage_ziliao);
+        mypage_name = view.findViewById(R.id.mypage_name);
+        mPreferences = getActivity().getSharedPreferences("selfinformation", Context.MODE_PRIVATE);
+        mypage_name.setText(mPreferences.getString("name",""));
+        mEditor = mPreferences.edit();
+        edit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final AlertDialog.Builder alertDialog7 = new AlertDialog.Builder(getActivity());
+                View view1 = View.inflate(getContext(), R.layout.activity_self_information, null);
+                commit = view1.findViewById(R.id.commit);
+                name = view1.findViewById(R.id.name);
+                say = view1.findViewById(R.id.say);
+                fale = view1.findViewById(R.id.fale);
+                if(mPreferences.getString("name","").equals("")){}
+                else name.setText(mPreferences.getString("name",""));
+                if(mPreferences.getString("say","").equals("")){}
+                else say.setText(mPreferences.getString("say",""));
+                if(mPreferences.getString("fale","").equals("")){}
+                else fale.setText(mPreferences.getString("fale",""));
+                alertDialog7.setTitle("编辑信息")
+                        .setIcon(R.mipmap.ic_launcher)
+                        .setView(view1)
+                        .create();
+                final AlertDialog show = alertDialog7.show();
+
+                commit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(fale.getText().toString().equals("")){}
+                        else mEditor.putString("fale",fale.getText().toString());
+                        if(name.getText().toString().equals("")){}
+                        else mEditor.putString("name",name.getText().toString());
+                        if(say.getText().toString().equals("")){}
+                        else mEditor.putString("say",say.getText().toString());
+                        mEditor.commit();
+                        show.dismiss();
+                    }
+                });
+
+
+            }
+        });
         return view;
     }
     private void initView() {
